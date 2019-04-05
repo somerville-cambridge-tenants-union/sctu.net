14708b227c285a8458a8795491328d959ba28edd
Alexander Mancevice <amancevice@cargometrics.com>
2019-04-05 17:15:45 -0400

    sctu.net => www.sctu.net

--- terraform.tf
+++ terraform.tf
@@ -1,16 +1,43 @@
 provider aws {
-  profile = "mcc"
-  region  = "us-east-1"
-  version = "~> 2.4"
+  access_key = "${var.aws_access_key_id}"
+  profile    = "${var.aws_profile}"
+  region     = "${var.aws_region}"
+  secret_key = "${var.aws_secret_access_key}"
+  version    = "~> 2.5"
 }
 
-resource aws_acm_certificate cert {
-  domain_name       = "${aws_s3_bucket.sctu.bucket}"
-  validation_method = "DNS"
+locals {
+  domain_name = "sctu.net"
+  html        = ["error", "index"]
+
+  tags {
+    App     = "sctu"
+    Name    = "${local.domain_name}"
+    Release = "${var.release}"
+    Repo    = "${var.repo}"
+  }
 }
 
-resource aws_cloudfront_distribution s3_distribution {
-  aliases             = ["sctu.net", "www.sctu.net"]
+data aws_iam_policy_document website {
+  statement {
+    sid       = "1"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::www.${local.domain_name}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.website.iam_arn}"]
+    }
+  }
+}
+
+data aws_acm_certificate cert {
+  domain   = "${local.domain_name}"
+  statuses = ["ISSUED"]
+}
+
+resource aws_cloudfront_distribution website {
+  aliases             = ["${local.domain_name}", "www.${local.domain_name}"]
   default_root_object = "index.html"
   enabled             = true
   is_ipv6_enabled     = true
@@ -29,7 +56,7 @@ resource aws_cloudfront_distribution s3_distribution {
     default_ttl            = 86400
     max_ttl                = 31536000
     min_ttl                = 0
-    target_origin_id       = "${aws_s3_bucket.sctu.bucket}"
+    target_origin_id       = "${aws_s3_bucket.website.bucket}"
     viewer_protocol_policy = "redirect-to-https"
 
     forwarded_values {
@@ -42,8 +69,12 @@ resource aws_cloudfront_distribution s3_distribution {
   }
 
   origin {
-    domain_name = "${aws_s3_bucket.sctu.bucket_regional_domain_name}"
-    origin_id   = "${aws_s3_bucket.sctu.bucket}"
+    domain_name = "${aws_s3_bucket.website.bucket_regional_domain_name}"
+    origin_id   = "${aws_s3_bucket.website.bucket}"
+
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.website.cloudfront_access_identity_path}"
+    }
   }
 
   restrictions {
@@ -53,14 +84,135 @@ resource aws_cloudfront_distribution s3_distribution {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = "${aws_acm_certificate.cert.arn}"
+    acm_certificate_arn      = "${data.aws_acm_certificate.cert.arn}"
     minimum_protocol_version = "TLSv1.1_2016"
     ssl_support_method       = "sni-only"
   }
 }
 
-resource aws_s3_bucket sctu {
-  acl           = "public-read"
-  bucket        = "sctu.net"
+resource aws_cloudfront_origin_access_identity website {
+  comment = "access-identity-www.${local.domain_name}.s3.amazonaws.com"
+}
+
+/*
+resource aws_route53_record a {
+  name    = "${local.domain_name}"
+  type    = "A"
+  zone_id = "${aws_route53_zone.website.id}"
+
+  alias {
+    evaluate_target_health = false
+    name                   = "${aws_cloudfront_distribution.website.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.website.hosted_zone_id}"
+  }
+}
+
+resource aws_route53_record aaaa {
+  name    = "${local.domain_name}"
+  type    = "AAAA"
+  zone_id = "${aws_route53_zone.website.id}"
+
+  alias {
+    evaluate_target_health = false
+    name                   = "${aws_cloudfront_distribution.website.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.website.hosted_zone_id}"
+  }
+}
+
+resource aws_route53_record www_a {
+  name    = "www.${local.domain_name}"
+  type    = "A"
+  zone_id = "${aws_route53_zone.website.id}"
+
+  alias {
+    evaluate_target_health = false
+    name                   = "${aws_cloudfront_distribution.website.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.website.hosted_zone_id}"
+  }
+}
+
+resource aws_route53_record www_aaaa {
+  name    = "www.${local.domain_name}"
+  type    = "AAAA"
+  zone_id = "${aws_route53_zone.website.id}"
+
+  alias {
+    evaluate_target_health = false
+    name                   = "${aws_cloudfront_distribution.website.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.website.hosted_zone_id}"
+  }
+}
+*/
+
+resource aws_s3_bucket website {
+  acl           = "private"
+  bucket        = "www.${local.domain_name}"
   force_destroy = false
+  policy        = "${data.aws_iam_policy_document.website.json}"
+  tags          = "${local.tags}"
+
+  website {
+    error_document = "error.html"
+    index_document = "index.html"
+  }
+}
+
+resource aws_s3_bucket_object html {
+  count        = "${length(local.html)}"
+  acl          = "private"
+  bucket       = "${aws_s3_bucket.website.bucket}"
+  content      = "${file("${local.domain_name}/${element(local.html, count.index)}.html")}"
+  content_type = "text/html"
+  etag         = "${filemd5("${local.domain_name}/${element(local.html, count.index)}.html")}"
+  key          = "${element(local.html, count.index)}.html"
+  tags         = "${local.tags}"
+}
+
+resource aws_s3_bucket_object png {
+  acl          = "private"
+  bucket       = "${aws_s3_bucket.website.bucket}"
+  content_type = "image/jpeg"
+  etag         = "${filemd5("${local.domain_name}/logo.jpg")}"
+  key          = "logo.jpg"
+  source       = "${local.domain_name}/logo.jpg"
+  tags         = "${local.tags}"
+}
+
+resource aws_s3_bucket_object otf {
+  acl          = "private"
+  bucket       = "${aws_s3_bucket.website.bucket}"
+  content_type = "application/x-font-opentype"
+  etag         = "${filemd5("${local.domain_name}/MonumentExtended-Ultrabold.otf")}"
+  key          = "MonumentExtended-Ultrabold.otf"
+  source       = "${local.domain_name}/MonumentExtended-Ultrabold.otf"
+  tags         = "${local.tags}"
+}
+
+variable aws_access_key_id {
+  description = "AWS Access Key ID."
+  default     = ""
+}
+
+variable aws_secret_access_key {
+  description = "AWS Secret Access Key."
+  default     = ""
+}
+
+variable aws_profile {
+  description = "AWS Profile."
+  default     = ""
+}
+
+variable aws_region {
+  description = "AWS Region."
+  default     = "us-east-1"
+}
+
+variable release {
+  description = "Release tag."
+}
+
+variable repo {
+  description = "Project repository."
+  default     = "https://github.com/somerville-cambridge-tenants-union/sctu.net"
 }
