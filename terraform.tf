72315f38a0a29a77e3ae4016293b71fed317dc5d
Alexander Mancevice <amancevice@cargometrics.com>
2019-10-24 17:04:04 -0400

    Add public block on S3 bucket

--- terraform.tf
+++ terraform.tf
@@ -199,6 +199,14 @@ resource aws_s3_bucket website {
   }
 }
 
+resource aws_s3_bucket_public_access_block website {
+  bucket                  = aws_s3_bucket.website.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource null_resource sync {
   triggers = {
     digest = file("www.sha256sum")
